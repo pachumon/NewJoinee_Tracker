9120430d4b84b0b823a433c10921c744072258f8
Prasanth Shenoy <pshenoy@firstam.com>
2020-06-17 07:28:16 +0530

    Jquery based Model Validation client side

--- FAI.NewJoineeTracker/FAI.NewJoineeTracker.Web/Models/View/NewJoineeInfo.cs
+++ FAI.NewJoineeTracker/FAI.NewJoineeTracker.Web/Models/View/NewJoineeInfo.cs
@@ -10,13 +10,22 @@ namespace FAI.NewJoineeTracker.Web.Models.View
     {
         public int EmployeeId { get; set; }
         [Required]
+        [MinLength(5)]
+        [MaxLength(30)]
         public string Name { get; set; }
         public DateTime DateOfJoining { get; set; }
         [Required]
+        [MinLength(3,ErrorMessage ="Should have a minimum of 3 characters")]
+        [MaxLength(30)]
         public string Project { get; set; }
         [Required]
+        [MinLength(6)]
+        [MaxLength(30)]
         public string Location { get; set; }
+
         [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
         public string BusinessUnit { get; set; }
         public bool IsActive { get; set; }
 
