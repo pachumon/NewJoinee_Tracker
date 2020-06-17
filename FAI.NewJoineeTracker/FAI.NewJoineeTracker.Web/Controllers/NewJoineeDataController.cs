372d7aab6db4b513c821107c29073793e1829e24
Prasanth Shenoy <pshenoy@firstam.com>
2020-06-17 08:41:36 +0530

    Api Controllers and the Client side Drift

---
+++ FAI.NewJoineeTracker/FAI.NewJoineeTracker.Web/Controllers/NewJoineeDataController.cs
@@ -0,0 +1,26 @@
+﻿using FAI.NewJoineeTracker.Web.Models.View;
+using FAI.NewJoineeTracker.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FAI.NewJoineeTracker.Web.Controllers
+{
+    public class NewJoineeDataController : ApiController
+    {
+        private readonly NewJoineeInfoService newJoineeInfoService = null;
+
+        public NewJoineeDataController()
+        {
+            newJoineeInfoService = new NewJoineeInfoService();
+        }
+        public IEnumerable<NewJoineeInfo> GetNewJoinees()
+        {
+            var newJoineeLst = newJoineeInfoService.GetAllNewJoinees();
+            return newJoineeLst;
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[EmployeeId] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(30) NOT NULL, 
+    [Project] NCHAR(30) NOT NULL, 
+    [Location] NCHAR(30) NOT NULL, 
+    [BusinessUnit] NCHAR(30) NOT NULL, 
+    [DateOfJoining] DATETIME NOT NULL, 
+    [IsActive] BIT NOT NULL
+)
