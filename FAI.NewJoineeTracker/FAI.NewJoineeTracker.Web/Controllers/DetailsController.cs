806d59c9f722120331e0c7ae3de32f0b208f9e2c
Prasanth Shenoy <pshenoy@firstam.com>
2020-06-17 05:58:32 +0530

    new controller and simple routing with params

---
+++ FAI.NewJoineeTracker/FAI.NewJoineeTracker.Web/Controllers/DetailsController.cs
@@ -0,0 +1,24 @@
+﻿using FAI.NewJoineeTracker.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FAI.NewJoineeTracker.Web.Controllers
+{
+    public class DetailsController : Controller
+    {
+        private readonly NewJoineeInfoService newJoineeInfoService = null;
+        public DetailsController()
+        {
+            newJoineeInfoService = new NewJoineeInfoService();
+        }
+        // GET: Details
+        public ActionResult Index(int joineeId)
+        {
+            var newJoineeInfo = newJoineeInfoService.GetNewJoineeInfo(joineeId);
+            return View(newJoineeInfo);
+        }
+    }
+}
